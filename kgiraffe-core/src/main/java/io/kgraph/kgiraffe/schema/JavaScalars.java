92d7a06737b4953689f4a11bac18f8c4503e7a70
Robert Yokota <rayokota@gmail.com>
2022-10-04 17:20:53 -0700

    Add JsonPrimitive GraphQL type

--- kgiraffe-core/src/main/java/io/kgraph/kgiraffe/schema/JavaScalars.java
+++ kgiraffe-core/src/main/java/io/kgraph/kgiraffe/schema/JavaScalars.java
@@ -1,5 +1,10 @@
 package io.kgraph.kgiraffe.schema;
 
+import graphql.language.BooleanValue;
+import graphql.language.EnumValue;
+import graphql.language.FloatValue;
+import graphql.language.IntValue;
+import graphql.language.NullValue;
 import graphql.language.StringValue;
 import graphql.language.Value;
 import graphql.schema.Coercing;
@@ -10,6 +15,8 @@ import graphql.schema.GraphQLScalarType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -22,6 +29,7 @@ public class JavaScalars {
     private static final Logger LOG = LoggerFactory.getLogger(JavaScalars.class);
 
     public static final GraphQLScalarType GraphQLDate;
+    public static final GraphQLScalarType GraphQLJsonPrimitive;
     public static final GraphQLScalarType GraphQLVoid;
 
     private static final String ISO_8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
@@ -98,6 +106,90 @@ public class JavaScalars {
             .name("DateTime")
             .description("An RFC-3339 compliant DateTime Scalar")
             .coercing(dateCoercing)
+            .build();
+
+        Coercing<Object, Object> jsonPrimitiveCoercing = new Coercing<Object, Object>() {
+            public Object serialize(Object input) throws CoercingSerializeException {
+                if (input == null
+                    || input instanceof String
+                    || input instanceof Number
+                    || input instanceof Boolean) {
+                    return input;
+                }
+                throw new CoercingSerializeException(
+                    "Expected something we can convert to a JSON primitive but was '" + typeName(input)
+                        + "'.");
+            }
+
+            public Object parseValue(Object input) throws CoercingParseValueException {
+                if (input == null
+                    || input instanceof String
+                    || input instanceof Number
+                    || input instanceof Boolean) {
+                    return input;
+                }
+                throw new CoercingParseValueException(
+                    "Expected a JSON primitive but was '" + typeName(input) + "'.");
+            }
+
+            public Object parseLiteral(Object input) throws CoercingParseLiteralException {
+                if (!(input instanceof Value)) {
+                    throw new CoercingParseLiteralException(
+                        "Expected AST type 'Value' but was '" + typeName(input) + "'."
+                    );
+                }
+                if (input instanceof FloatValue) {
+                    return ((FloatValue) input).getValue();
+                }
+                if (input instanceof StringValue) {
+                    return ((StringValue) input).getValue();
+                }
+                if (input instanceof IntValue) {
+                    return ((IntValue) input).getValue();
+                }
+                if (input instanceof BooleanValue) {
+                    return ((BooleanValue) input).isValue();
+                }
+                if (input instanceof EnumValue) {
+                    return ((EnumValue) input).getName();
+                }
+                throw new CoercingParseLiteralException(
+                    "Expected AST type of primitive value but was '" + typeName(input) + "'.");
+            }
+
+            public Value<?> valueToLiteral(Object input) {
+                if (input == null) {
+                    return NullValue.newNullValue().build();
+                }
+                if (input instanceof String) {
+                    return new StringValue((String) input);
+                }
+                if (input instanceof Float) {
+                    return new FloatValue(BigDecimal.valueOf((Float) input));
+                }
+                if (input instanceof Double) {
+                    return new FloatValue(BigDecimal.valueOf((Double) input));
+                }
+                if (input instanceof BigDecimal) {
+                    return new FloatValue((BigDecimal) input);
+                }
+                if (input instanceof BigInteger) {
+                    return new IntValue((BigInteger) input);
+                }
+                if (input instanceof Number) {
+                    long l = ((Number) input).longValue();
+                    return new IntValue(BigInteger.valueOf(l));
+                }
+                if (input instanceof Boolean) {
+                    return new BooleanValue((Boolean) input);
+                }
+                throw new UnsupportedOperationException("Can't handle values of type: " + typeName(input));
+            }
+        };
+        GraphQLJsonPrimitive = GraphQLScalarType.newScalar()
+            .name("JsonPrimitive")
+            .description("A JSON Primitive Scalar")
+            .coercing(jsonPrimitiveCoercing)
             .build();
 
         Coercing<Void, Void> voidCoercing = new Coercing<>() {
